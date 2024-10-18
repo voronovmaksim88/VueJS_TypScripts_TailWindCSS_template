94fdca0b123643b61617aab6f2b2665b8f6e6797
Maksim <voronovmaksim88@gmail.com>
2024-10-18 10:43:15 +0700

    Initial commit

---
+++ my-vue-ts-project/src/components/TheMyHelloWorld.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    const count = ref(0)
+    const increment = () => {
+      count.value++
+    }
+
+    return { count, increment }
+  }
+})
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+/* Ваши стили */
+</style>
